6e4c8a332df549e18a51222a94f6bea26d3b2851
Lakshay Mittal <lakshaymittal225@gmail.com>
2023-04-19 15:54:54 +0530

    tests: Add test to extract data without specifying `start` and `end`.

--- tests/tests.py
+++ tests/tests.py
@@ -9,6 +9,10 @@ class TestExtractDataFromPdf(unittest.TestCase):
             "tests/annual_reports/sbi.pdf", start=119, end=119
         )
         self.assertEqual(expected, actual)
+        actual = extract_data_from_pdf(
+            "tests/annual_reports/sbi.pdf", statement_name="balance sheet"
+        )
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
