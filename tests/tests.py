f13a5c840470f419cd4f134ad7407db16673e867
Lakshay Mittal <lakshaymittal225@gmail.com>
2023-07-15 14:55:39 +0530

    tests: Add test for testing `tablex` with `SBI Balanace Sheet`.

--- tests/tests.py
+++ tests/tests.py
@@ -2,18 +2,29 @@ import json
 import unittest
 
 from analyst import extract_data_from_pdf
+from utilities import tablex
+
+
+class TestTablex(unittest.TestCase):
+    def test_tablex_sbi(self):
+        with open("tests/expected-table/sbi-balance-sheet.json", "r") as f:
+            expected = json.load(f)
+        actual = tablex.extract_tables(
+            "tests/annual-reports/sbi.pdf", start=119, end=119
+        )
+        self.assertEqual(expected, actual)
 
 
 class TestExtractDataFromPdf(unittest.TestCase):
     def test_extract_data_from_pdf_sbi(self):
-        with open("tests/expected_data/sbi-balance-sheet.json", "r") as f:
+        with open("tests/expected-data/sbi-balance-sheet.json", "r") as f:
             expected = json.load(f)
         actual = extract_data_from_pdf(
-            "tests/annual_reports/sbi.pdf", start=119, end=119
+            "tests/annual-reports/sbi.pdf", start=119, end=119
         )
         self.assertEqual(expected, actual)
         actual = extract_data_from_pdf(
-            "tests/annual_reports/sbi.pdf", statement_name="balance sheet"
+            "tests/annual-reports/sbi.pdf", statement_name="balance sheet"
         )
         self.assertEqual(expected, actual)
 
